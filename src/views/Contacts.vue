e1e0ca1403186d2f6e1d50c3d52c38378159cc4c
Nour Al Hassan <Nour.AlHassan@Student.HTW-Berlin.de>
2022-06-21 12:43:37 +0200

    Backend URL umgebungsabhängig verwenden

--- src/views/Contacts.vue
+++ src/views/Contacts.vue
@@ -51,3 +51,69 @@ export default {
   }
 }
 </script>
+<style scoped>
+.main {
+  padding: 50px;
+  min-height: 100px;
+  width:100%;
+  overflow: hidden;
+  background-color: #fff;
+  display: flex;
+  justify-content: space-around;
+  flex-wrap: wrap;
+  align-items: center;
+  border-radius: 5px;
+}
+.card {
+  padding: 10px;
+  margin: 10px;
+  border: #000 2px solid;
+}
+.card-img-top {
+  flex: 0.3;
+  height: 100px;
+  width: 100%;
+
+  padding: 20px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-right: 1px solid #f2f0f1;
+}
+.card-img-top {
+  width: 100px ;
+  border-radius: 50%;
+}
+.contact {
+  flex: 0.7;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  padding: 20px;
+}
+.contact > * {
+  margin: 0;
+}
+
+.card-title {
+  margin-bottom: 15px;
+}
+
+.contact__btn {
+  background-color: #398a13;
+  border: none;
+  outline: none;
+  width: 150px;
+  padding: 10px;
+  color: #fff;
+  margin-bottom: 10px;
+}
+.delete__btn {
+  background-color: #c81811;
+  border: none;
+  outline: none;
+  width: 150px;
+  padding: 10px;
+  color: #fff;
+}
+</style>
