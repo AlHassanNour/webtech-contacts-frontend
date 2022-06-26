a44608778ef368413344ce984e5f47ace5268f5f
Nour Al Hassan <Nour.AlHassan@Student.HTW-Berlin.de>
2022-06-26 19:44:00 +0200

    neue contact über das frontend anlegen

--- src/components/ContactCardList.vue
+++ src/components/ContactCardList.vue
@@ -1,24 +1,26 @@
 <template>
-  <h1> Welcome to Contacts</h1>
-  <div class="container-fluid" >
-    <contact-card-list :contacts="this.contacts"></contact-card-list>
-  </div>
-  <ContactsCreateForm></ContactsCreateForm>
+    <div class="row row-cols-1 row-cols-md-3 g-4">
+      <div class="col" v-for="contact in contacts" :key="contact.id">
+        <div class="card h-100">
+          <img :src="getAvatar(contact)" class="card-img-top" :alt="contact.firstName + ' ' + contact.secondName">
+          <div class="card-body">
+            <h5 class="card-title">{{contact.firstName}} {{contact.secondName}}</h5>
+            <p class="card-text">Work : {{contact.work}}</p>
+            <p class="card-text">E-Mail : {{contact.email}}</p>
+            <p class="card-text">Phone : {{contact.phone}}</p>
+          </div>
+        </div>
+      </div>
+    </div>
 </template>
 
 <script>
-import ContactCardList from '@/components/ContactCardList'
-import ContactsCreateForm from '@/components/ContactsCreateForm'
 export default {
-  // eslint-disable-next-line
-  name: 'Contacts',
-  components: {
-    ContactCardList,
-    ContactsCreateForm
-  },
-  data () {
-    return {
-      contacts: []
+  name: 'ContactCardList',
+  props: {
+    contacts: {
+      type: Array,
+      required: true
     }
   },
   methods: {
@@ -29,20 +31,6 @@ export default {
         return require('../assets/FEMALE.png')
       }
     }
-  },
-  mounted () {
-    const endpoint = process.env.VUE_APP_BACKEND_BASE_URL + '/api/v1/contacts'
-    const requestOptions = {
-      method: 'GET',
-      redirect: 'follow'
-    }
-
-    fetch(endpoint, requestOptions)
-      .then(response => response.json())
-      .then(result => result.forEach(contact => {
-        this.contacts.push(contact)
-      }))
-      .catch(error => console.log('error', error))
   }
 }
 </script>
---
+++ src/components/ContactsCreateForm.vue
@@ -0,0 +1,121 @@
+<template>
+  <button class="btn btn-success sticky-button" data-bs-toggle="offcanvas" data-bs-target="#contacts-create-offcanvas" aria-controls="#contacts-create-offcanvas">
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-plus-fill" viewBox="0 0 16 16">
+      <path d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
+      <path fill-rule="evenodd" d="M13.5 5a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0V8h-1.5a.5.5 0 0 1 0-1H13V5.5a.5.5 0 0 1 .5-.5z"/>
+    </svg>
+  </button>
+  <div class="offcanvas offcanvas-end" tabindex="1" id="contacts-create-offcanvas" aria-labelledby="offcanvas-label">
+    <div class="offcanvas-header">
+      <h5 id="offcanvas-label">New Contact</h5>
+      <button type="button" id="close-offcanvas" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+    </div>
+    <div class="offcanvas-body">
+  <form class="text-start needs-validation" id="contacts-create-form" novalidate>
+    <div class="mb-3">
+      <label for="first-name" class="form-label">First Name</label>
+      <input type="text" class="form-control" id="first-name" v-model="firstName" required>
+      <div class="invalid-feedback">
+        Please provide the first name.
+      </div>
+    </div>
+    <div class="mb-3">
+      <label for="second-name" class="form-label">Second Name</label>
+      <input type="text" class="form-control" id="second-name" v-model="secondName" required>
+      <div class="invalid-feedback">
+        Please provide the second name.
+      </div>
+    </div>
+      <div class="mb-3">
+        <label for="phone" class="form-label">Phone</label>
+        <input type="text" class="form-control" id="phone" v-model="phone" required>
+        <div class="invalid-feedback">
+          Please provide the phone.
+        </div>
+      </div>
+      <div class="mb-3">
+        <label for="work" class="form-label"> Work</label>
+        <input type="text" class="form-control" id="work" v-model="work" required>
+        <div class="invalid-feedback">
+          Please provide the work.
+        </div>
+      </div>
+    <div class="mb-3">
+      <label for="email" class="form-label"> E-Mail</label>
+      <input type="text" class="form-control" id="email" v-model="email" required>
+      <div class="invalid-feedback">
+        Please provide the email.
+      </div>
+    </div>
+    <div class="mb-3">
+      <label for="gender" class="form-label">Gender</label>
+      <select id="gender" class="form-select" v-model="gender" required>
+        <option value="" selected disabled>Choose...</option>
+        <option value="MALE">Male</option>
+        <option value="FEMALE">Female</option>
+        <option value="DIVERSE">Diverse</option>
+      </select>
+      <div class="invalid-feedback">
+        Please select a valid gender.
+      </div>
+    </div>
+    <div class="mt-5">
+      <button class="btn btn-primary me-3" type="submit" @click="createContact">Create</button>
+      <button class="btn btn-danger" type="reset">Reset</button>
+    </div>
+  </form>
+    </div>
+  </div>
+</template>
+<script>
+
+export default {
+  name: 'ContactsCreateForm',
+  data () {
+    return {
+      firstName: '',
+      secondName: '',
+      gender: '',
+      email: '',
+      work: '',
+      phoneNumber: ''
+    }
+  },
+  methods: {
+    createContact: function () {
+      const endpoint = process.env.VUE_APP_BACKEND_BASE_URL + '/api/v1/contacts'
+      const headers = new Headers()
+      headers.append('Content-Type', 'application/json')
+
+      const payload = JSON.stringify({
+        firstName: this.firstName,
+        secondName: this.secondName,
+        gender: this.gender,
+        email: this.email,
+        work: this.work,
+        phone: this.phone
+      })
+
+      const requestOptions = {
+        method: 'POST',
+        headers: headers,
+        body: payload,
+        redirect: 'follow'
+      }
+
+      fetch(endpoint, requestOptions)
+        .catch(error => console.log('error', error))
+    }
+  }
+}
+</script>
+
+<style scoped>
+.sticky-button {
+  position: fixed;
+  bottom: 20px;
+  right: 20px;
+  padding: 10px 15px;
+  border-radius: 30px;
+}
+</style>
